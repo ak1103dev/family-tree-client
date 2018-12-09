c9a95c9ad92486fc901c7d594471cc9efaf57671
ak1103dev <ak.apichan1103@gmail.com>
2018-12-09 12:49:52 +0700

    :construction: login page

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-row>
+    <el-col :span="12" :offset="6">
+      <h1 style="text-align:center">Login Page</h1>
+      <el-form ref="form" :model="form" label-width="120px">
+        <el-form-item label="Username">
+          <el-input v-model="form.username"></el-input>
+        </el-form-item>
+        <el-form-item label="Password">
+          <el-input v-model="form.password" type="password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">Login</el-button>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log("submit!");
+      this.$router.push({ name: "about" });
+    }
+  }
+};
+</script>
