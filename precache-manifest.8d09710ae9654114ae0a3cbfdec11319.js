60941742783bc46015457d448781721d8237b627
ak1103dev <ak.apichan1103@gmail.com>
2018-12-09 21:04:17 +0700

    deploy

---
+++ precache-manifest.8d09710ae9654114ae0a3cbfdec11319.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/family-tree-client/robots.txt"
+  },
+  {
+    "revision": "8b9c1afaab90d8189675",
+    "url": "/family-tree-client/js/chunk-vendors.b07418fa.js"
+  },
+  {
+    "revision": "fd67465c06bdce434dd6",
+    "url": "/family-tree-client/js/app.cd921f44.js"
+  },
+  {
+    "revision": "5691f97104e480bb6881",
+    "url": "/family-tree-client/js/about.5c489012.js"
+  },
+  {
+    "revision": "8c44d3f19dac999dbd2d8e3996c29b20",
+    "url": "/family-tree-client/index.html"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/family-tree-client/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/family-tree-client/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "fd67465c06bdce434dd6",
+    "url": "/family-tree-client/css/app.7df5d571.css"
+  }
+];
